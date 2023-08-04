376fac8793020dc0d84cc53f6e0dcc563cef6f19
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-08-04 17:42:04 -0400

    Use doppler token

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe', appEnv: process.env.APP_ENV })
+const https = require('https')
+
+module.exports.getSecrets = async () => {
+  return new Promise(function(resolve, reject) {
+    https.get(`https://${process.env.DOPPLER_TOKEN}@api.doppler.com/v3/configs/config/secrets/download?format=json`, (res) => {
+      let secrets = ''
+      res.on('data', (data) => secrets += data);
+      res.on('end', () => resolve(JSON.parse(secrets)))
+    }).on('error', (e) => reject(e))
+  })
+}
+
+export default async function handler(req, res) {
+  res.status(200).json({ name: 'John Doe', appEnv: process.env.APP_ENV, secrets: await this.getSecrets() })
 }
