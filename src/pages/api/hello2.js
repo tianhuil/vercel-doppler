025f81e475d6ff616375baaf03225961d73416f6
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-08-04 17:47:40 -0400

    More endpoints

---
+++ src/pages/api/hello2.js
@@ -0,0 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+  res.status(200).json({ name: 'John Doe', appEnv: process.env.APP_ENV, doppler: !!process.env.DOPPLER_TOKEN, foo: process.env.FOO })
+}
